d809472cc79866a26b941e6709d313db9eef4d36
Caraliputia <montigny49@yahoo.com>
2026-08-19 14:38:40 +0200

    Add smooth scroll to back-to-top button

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ if (toggle && nav) {
     toggle.setAttribute('aria-expanded', 'false');
   }));
 }
+
+const backToTopButton = document.getElementById("back-to-top");
+
+if (backToTopButton) {
+  backToTopButton.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth"
+    });
+  });
+}
